c9582caebab54ff6e02fb38500df14663874fac7
Dayam Zaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2021-05-27 10:30:36 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+//(function() {
+
+    // Initialize Firebase
+    var Config = {
+      apiKey: "AIzaSyD8pSg4WcoA8pOBIH-PQez6jmDT9wJQitE",
+      authDomain: "instagram-ead4b.firebaseapp.com",
+      databaseURL: "https://instagram-ead4b-default-rtdb.firebaseio.com",
+      projectId: "instagram-ead4b",
+      storageBucket: "instagram-ead4b.appspot.com",
+      messagingSenderId: "870577544725",
+      appId: "1:870577544725:web:f77bef08bb80d2265f381c",
+      measurementId: "G-99XJVESS7T"
+    };
+  
+  firebase.initializeApp(firebaseConfig);
+//})();
